ec686583357cd1ccb7d0e4287e8e002efa8758e7
XinRan Hao <xrhao@freewheel.tv>
2015-09-05 17:56:17 +0800

    added boxlayer

--- src/BackgroundLayer.js
+++ src/BackgroundLayer.js
@@ -1,11 +1,12 @@
 var BackgroundLayer = cc.Layer.extend({
-  ctor:function () {
+  ctor : function () {
     this._super();
     this.init();
   },
 
-  init:function () {
+  init : function () {
     this._super();
+
     var winsize = cc.director.getWinSize();
     var widthcenter = winsize.width / 2;
     var hightcenter = winsize.height / 2;
@@ -14,35 +15,7 @@ var BackgroundLayer = cc.Layer.extend({
     var spriteBG = new cc.Sprite(res.HelloWorld_png);
     spriteBG.setPosition(centerPos);
     this.addChild(spriteBG);
-    
-    editBox = [];
-    for(var i = 0; i < 3; i++){
-        editBox[i] = new MinionBox(
-        res.start_n_png,
-        res.start_s_png,
-        "edit",
-        i * 10
-      );
-      editBox[i].setPosition(widthcenter + 200 * (i-1), hightcenter);
-      this.addChild(editBox[i]);
-    };
-
-    var sumUp = new cc.MenuItemSprite(
-      new cc.Sprite(res.start_n_png),
-      new cc.Sprite(res.start_s_png),
-      this.onSum,
-      this
-    );
-    var menu = new cc.Menu(sumUp);
-    menu.setPosition(widthcenter, hightcenter - 120);
-    this.addChild(menu);
-  },
-
-  onSum : function () {
-    var sum = 0;
-    for(var i = 0; i < 3; i++){
-  	  sum += editBox[i].getCost();
-    }
+  
   }
 
 });
--- src/MinionScene.js
+++ src/MinionScene.js
@@ -2,5 +2,6 @@ var MinionScene = cc.Scene.extend({
 	onEnter:function () {
 		this._super();
 		this.addChild(new BackgroundLayer());
+    this.addChild(new BoxLayer());
 	}
 });
