abfa2143dcc0f5cc64294b7b35343eb1afcd7a43
XinRan Hao <xrhao@freewheel.tv>
2015-09-05 17:27:45 +0800

    added minion boxes

--- src/BackgroundLayer.js
+++ src/BackgroundLayer.js
@@ -7,39 +7,42 @@ var BackgroundLayer = cc.Layer.extend({
   init:function () {
     this._super();
     var winsize = cc.director.getWinSize();
-
-    var centerPos = cc.p(winsize.width / 2, winsize.height / 2);
+    var widthcenter = winsize.width / 2;
+    var hightcenter = winsize.height / 2;
+    var centerPos = cc.p(widthcenter, hightcenter);
+    
     var spriteBG = new cc.Sprite(res.HelloWorld_png);
     spriteBG.setPosition(centerPos);
     this.addChild(spriteBG);
     
-    
-    var editBox = [];
+    editBox = [];
     for(var i = 0; i < 3; i++){
-      editBox[i] = new cc.EditBox(
-      	new cc.Size(50, 50),
-      	new cc.Scale9Sprite(res.start_n_png), // normal state image
-      	new cc.Scale9Sprite(res.start_s_png), //select state image
-      	new cc.Scale9Sprite(res.start_s_png)  //disabled state image
+        editBox[i] = new MinionBox(
+        res.start_n_png,
+        res.start_s_png,
+        "edit",
+        i * 10
       );
-      editBox[i].setPosition(150, winsize.height - (i+1) * 100);
+      editBox[i].setPosition(widthcenter + 200 * (i-1), hightcenter);
       this.addChild(editBox[i]);
-    }
-    
-    var sumUp = new cc.MenuItemSprite(
-    	new cc.Sprite(res.start_n_png),
-    	new cc.Sprite(res.start_s_png),
-    	this.onSum, this);
-    sumUp.setPosition(250, winsize.height - 300);
+    };
 
+    var sumUp = new cc.MenuItemSprite(
+      new cc.Sprite(res.start_n_png),
+      new cc.Sprite(res.start_s_png),
+      this.onSum,
+      this
+    );
     var menu = new cc.Menu(sumUp);
+    menu.setPosition(widthcenter, hightcenter - 120);
     this.addChild(menu);
-    
   },
 
-  onSum:function (editBox) {
-	var sum = 0;
+  onSum : function () {
+    var sum = 0;
+    for(var i = 0; i < 3; i++){
+  	  sum += editBox[i].getCost();
+    }
   }
-
 
 });
