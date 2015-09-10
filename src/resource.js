d80c8267487fb822c5909bb2f957e446079d675a
Ye Zhu <yezhu@freewheel.tv>
2015-09-10 16:59:20 +0800

    modify resource.js

--- src/resource.js
+++ src/resource.js
@@ -15,18 +15,35 @@ var res = {
     BTBackSelected : "res/button/bt_back_selected.png",
     BTRPTNormal : "res/button/bt_rpt_normal.png",
     BTRPTSelected : "res/button/bt_rpt_selected.png",
+    BTReplayNormal : "res/button/bt_replay_normal.png",
+    BTReplaySelected : "res/button/bt_replay_selected.png",
     
     EditBoxBg : "res/edit_box_bg.png",
     EditBoxFront : "res/edit_box_front.png",
     IconBanana : "res/icon_banana.png",
 
-    Inventory1: "res/inventory/inventory_1.png",
-    Inventory2: "res/inventory/inventory_2.png",
-    Inventory3: "res/inventory/inventory_3.png",
-    Inventory4: "res/inventory/inventory_4.png",
-    Inventory5: "res/inventory/inventory_5.png",
-    Inventory6: "res/inventory/inventory_6.png",
-    Inventory7: "res/inventory/inventory_7.png"
+    Inventory1 : "res/inventory/inventory_1.png",
+    Inventory2 : "res/inventory/inventory_2.png",
+    Inventory3 : "res/inventory/inventory_3.png",
+    Inventory4 : "res/inventory/inventory_4.png",
+    Inventory5 : "res/inventory/inventory_5.png",
+    Inventory6 : "res/inventory/inventory_6.png",
+    Inventory7 : "res/inventory/inventory_7.png",
+
+    AdvBMW_plist : "res/plist/stuart_bmw.plist",
+    AdvBMW_png : "res/plist/stuart_bmw.png",
+    AdvCola_plist : "res/plist/jorge_cola.plist",
+    AdvCola_png : "res/plist/jorge_cola.png",
+    AdvBurger_plist : "res/plist/tim_burger.plist",
+    AdvBurger_png : "res/plist/tim_burger.png",
+
+    EndWin_plist : "res/plist/dave_win.plist",
+    EndWin_png : "res/plist/dave_win.png",
+    EndLose_plist : "res/plist/jorge_lose.plist",
+    EndLose_png : "res/plist/jorge_lose.png",
+
+    Hello_plist : "res/plist/dave_hello.plist",
+    Hello_png : "res/plist/dave_hello.png"
 };
 
 
